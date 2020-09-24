0416331dd8ce33a4657172841a363c8d434c2a5e
GeonwooVincentKim <kdsnop@gmail.com>
2020-09-24 16:53:50 +0900

    Reconstructed README.md file.

--- lib/app_screens/settings/dialog/Dialog.dart
+++ lib/app_screens/settings/dialog/Dialog.dart
@@ -28,3 +28,32 @@ void showAlertDialog(BuildContext context) async {
     },
   );
 }
+
+// 'ADD TO MY LIST' Dialog.
+void showAddListDialog(BuildContext context) async{
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+  String result = await showDialog(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('ADD TO MY LIST'),
+        content: Text("Are you sure you want to add TITLE OF THE GAME to your list of game?"),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.pop(context, "OK");
+            },
+          ),
+          FlatButton(
+            child: Text('Cancel'),
+            onPressed: () {
+              Navigator.pop(context, "Cancel");
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
