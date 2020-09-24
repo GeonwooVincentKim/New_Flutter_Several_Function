aede3881e39e4ae56dbd63f8347bb8d0322190ab
GeonwooVincentKim <kdsnop@gmail.com>
2020-09-24 16:48:27 +0900

    Last Commit of master

--- lib/app_screens/details/details_attributes/DetailsBody.dart
+++ lib/app_screens/details/details_attributes/DetailsBody.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/app_screens/settings/dialog/Dialog.dart';
 import 'DetailAttribute.dart';
 import 'package:flutter_app/app_screens/settings/SideMenu.dart';
 import 'package:flutter_app/shared/helpers/icomoon.dart';
@@ -13,13 +14,13 @@ class DetailPage extends StatelessWidget{
         centerTitle: true,
         actions: [
           IconButton(
-            icon: Icon(
-              IconMoon.iedit,
-              color: Colors.white,
-            ),
-            onPressed: (){
-              SideMenu();
-            }
+              icon: Icon(
+                IconMoon.iedit,
+                color: Colors.white,
+              ),
+              onPressed: (){
+                showAlertDialog(context);
+              }
           ),
         ],
       ),
