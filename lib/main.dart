4ce733e556bcc613d03c5639cbe001767d187d96
GeonwooVincentKim <kdsnop@gmail.com>
2020-09-25 14:04:30 +0900

    Successfully access to 'MyFavoritePage()'.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,22 @@ void main(){
       MaterialApp(
         title: "My Flutter App",
         home: Home(),
+        onGenerateRoute: (RouteSettings settings){
+          final List<String> pathElements = settings.name.split("/");
+          if(pathElements[0] != '') return null;
+          if(pathElements[1] == 'favorite'){
+            return MaterialPageRoute(
+              builder: (BuildContext context) => MyFavoritesPage(),
+            );
+          }
+          return null;
+        },
+        onUnknownRoute: (RouteSettings settings){
+          print(settings);
+          return MaterialPageRoute(
+            builder: (BuildContext context) => Home(),
+          );
+        },
         // routes: {
         //   '/': (context) => Home(),
         //   '/discover': (context) => DiscoverPage(),
