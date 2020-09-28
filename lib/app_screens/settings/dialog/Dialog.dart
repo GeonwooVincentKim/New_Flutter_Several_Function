a2a0a115ee40bb1a9a8cfb0455582674df66c6ab
GeonwooVincentKim <kdsnop@gmail.com>
2020-09-28 11:52:35 +0900

    Solved bugs that keep showing cannot use expanded top of the container.

--- lib/app_screens/settings/dialog/Dialog.dart
+++ lib/app_screens/settings/dialog/Dialog.dart
@@ -44,61 +44,59 @@ void showAddListDialog(BuildContext context) async{
         title: Text('ADD TO MY LIST', textAlign: TextAlign.center),
         titlePadding: EdgeInsets.symmetric(vertical: 20),
         contentPadding: EdgeInsets.zero,
-        content: Expanded(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25),
-                  child: Column(
-                    children: [
-                      Text("Are you sure you want to add TITLE OF THE GAME to your list of game?"),
-                      // CustomDivider(color: Colors.transparent),
-                      transparent_divider(),
-                      Row(
-                          children: [
-                            Expanded(
-                              child: Text("Progress: ", style: TextStyle(
-                                fontWeight: FontWeight.w300,
-                              )),
-                            ),
+        content: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  children: [
+                    Text("Are you sure you want to add TITLE OF THE GAME to your list of game?"),
+                    // CustomDivider(color: Colors.transparent),
+                    transparent_divider(),
+                    Row(
+                        children: [
+                          Expanded(
+                            child: Text("Progress: ", style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                            )),
+                          ),
 
-                            Expanded(
-                              child: TextField(
-                                textAlign: TextAlign.center,
-                                decoration: InputDecoration(
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(color: Colors.black87, width: 1.5),
-                                    ),
-                                    hintText: '10%'),
-                              ),
+                          Expanded(
+                            child: TextField(
+                              textAlign: TextAlign.center,
+                              decoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.black87, width: 1.5),
+                                  ),
+                                  hintText: '10%'),
                             ),
-                          ]
-                      ),
-                    ]
+                          ),
+                        ]
+                    ),
+                  ]
+                ),
+              ),
+              transparent_divider(),
+              InkWell(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  // padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: Text(
+                    "+ Add to my List",
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                transparent_divider(),
-                InkWell(
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    // padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    child: Text(
-                      "+ Add to my List",
-                      style: TextStyle(color: Colors.white),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       );
