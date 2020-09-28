c3b234ad95e14a9e0189590ffc511dd2f2bbf746
GeonwooVincentKim <kdsnop@gmail.com>
2020-09-28 17:05:36 +0900

    Successfully managed text-position.

--- lib/widgets/expanded/widgets_attribute/myfavorites/MyFavoritesWidget.dart
+++ lib/widgets/expanded/widgets_attribute/myfavorites/MyFavoritesWidget.dart
@@ -4,18 +4,22 @@ class FavoriteImage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ClipRRect(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10.0),
-          topRight: Radius.circular(10.0),
-        ),
-
-        // Make it fits to the box.
-        child: Image.network(
-            "https://www.gstatic.com/webp/gallery/1.jpg",
-            fit: BoxFit.fitWidth
-        ),
+      child: Image.network(
+          "https://www.gstatic.com/webp/gallery/1.jpg",
+          fit: BoxFit.fitWidth
       ),
+      // child: ClipRRect(
+      //   borderRadius: BorderRadius.only(
+      //     topLeft: Radius.circular(10.0),
+      //     topRight: Radius.circular(10.0),
+      //   ),
+      //
+      //   // Make it fits to the box.
+      //   child: Image.network(
+      //       "https://www.gstatic.com/webp/gallery/1.jpg",
+      //       fit: BoxFit.fitWidth
+      //   ),
+      // ),
     );
   }
 }
@@ -24,14 +28,29 @@ class FavoriteText extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      // width: MediaQuery.of(context).size.width,
-      // height: MediaQuery.of(context).size.height,
-      // padding: EdgeInsets.symmetric(vertical: 5.0),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.symmetric(vertical: 18),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(10.0),
-            bottomRight: Radius.circular(10.0)),
+        // borderRadius: BorderRadius.only(
+        //     bottomLeft: Radius.circular(10.0),
+        //     bottomRight: Radius.circular(10.0)),
         color: Colors.black12,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+              'TITLE OF THE GAME',
+              textScaleFactor: 2,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: "icomoon",
+                fontSize: 8,
+              )
+          ),
+        ],
       ),
     );
   }
