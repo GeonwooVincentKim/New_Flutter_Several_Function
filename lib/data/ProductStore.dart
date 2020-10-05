0b1cca206e57c9efa205fecc26ec245b0f4757a1
GeonwooVincentKim <kdsnop@gmail.com>
2020-10-05 14:19:19 +0900

    But I don't need to use stores.dart because I already have ProductStore.dart.

--- lib/data/ProductStore.dart
+++ lib/data/ProductStore.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/model/game/game.dart';
+
+class Cart with ChangeNotifier{
+  List<Game> _items = [];
+  List<Game> get items{
+    return [...items];
+  }
+
+}
