ba07c4dfc545c4769dd99862dd8a4b69c67f0404
GeonwooVincentKim <kdsnop@gmail.com>
2020-10-06 16:52:30 +0900

    Generated DiscoverAttributes.dart and make Discover.dart class as StatefulWidget.

--- lib/app_screens/discover/Discover.dart
+++ lib/app_screens/discover/Discover.dart
@@ -1,7 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/app_screens/details/Details.dart';
+import 'package:flutter_app/data/Provide.dart';
+import 'package:flutter_app/model/game/game.dart';
 import 'package:flutter_app/widgets/expanded/divider.dart';
 import 'package:flutter_app/widgets/expanded/widgets_attribute/Discover/DiscoverWidget.dart';
+import 'package:provider/provider.dart';
+
+class Discover extends StatefulWidget{
+  // final void Function(int) onAddButtonTapped;
+  // const Discover({Key key, this.onAddButtonTapped}) : super(key: key);
+
+  // @overide
+  State<StatefulWidget> createState() => _DiscoverState();
+}
+
+class _DiscoverState extends State<Discover>{
+  List<Game> pageList = [];
+
+  @override
+  void initState(){
+    final List<Game> listGame = Provider.of<Products>(context, listen: false).items;
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    var itemList = [
+      "Title_Details_1", "Title_Details_2", "Title_Details_3"
+    ];
+    return Center(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0),
+        color: Colors.black12,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                  child: ListView.separated(
+                      shrinkWrap: true,
+                      separatorBuilder: (context, index) =>
+                          transparent_divider(),
+                      itemCount: itemList.length,
+                      itemBuilder: (context, index) => DiscoverBodyDetails()
+                  )
+              ),
+            ]
+        ),
+      ),
+    );
+  }
+}
+
 
 class DiscoverPage extends StatelessWidget{
   @override
