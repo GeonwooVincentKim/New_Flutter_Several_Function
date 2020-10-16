0c5c00a8123eeef758baf9255a524807ec6ac3ab
GeonwooVincentKim <kdsnop@gmail.com>
2020-10-16 11:39:54 +0900

    Trying to refactor Main, Body, Home, Discover part.

--- lib/app_screens/Home.dart
+++ lib/app_screens/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_app/app_screens/Discover.dart';
 import 'package:flutter_app/app_screens/Body.dart';
 import 'package:flutter_app/app_screens/main/main_attributes/HomeAppBar.dart';
@@ -7,97 +8,28 @@ import 'package:flutter_app/shared/helpers/icomoon.dart';
 // ignore: implementation_imports
 import 'package:flutter/src/widgets/framework.dart';
 
-
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   final String title;
-
   Home({Key key, this.title}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState()
-  => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>{
+class _HomeState extends State<Home> {
   String _title;
   bool _showStar = true;
   int _selectedPage = 0;
-  List<Widget> pageList = List<Widget>();
-
-  @override
-  void initState() {
-    pageList.add(Body());
-    pageList.add(Discover());
-    _title = "MY GAMES";
-    super.initState();
-  }
-
-  List<BottomNavigationBarItem> buildBottomNavBarItems(){
-    return [
-      BottomNavigationBarItem(
-        icon: Icon(IconMoon.igamepad),
-        title: Text('My Games'),
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(IconMoon.isearch),
-        title: Text('Discover'),
-      ),
-    ];
-  }
 
   PageController pageController = PageController(
     initialPage: 0,
     keepPage: true,
   );
 
-  Widget buildPageView(){
-    return PageView(
-        controller: pageController,
-        onPageChanged: (index){
-          pageChanged(index);
-        },
-        //scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          Body(),
-          Discover(),
-        ]
-    );
-  }
-
   void pageChanged(int index){
     setState((){
       _selectedPage = index;
     });
-  }
-
-  void onAddButtonTapped(int index){
-    pageController.animateToPage(index);
-    pageController.jumpToPage(index);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: SideMenu(),
-      appBar: HomeAppbar(_title, _showStar, context),
-      body: buildPageView(),
-      bottomNavigationBar: BottomNavigationBar(
-
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white38,
-        // onTap: _onItemTapped,
-        iconSize: 50,
-        currentIndex: _selectedPage,
-        onTap: (index){
-          bottomTapped(index);
-        },
-        items: buildBottomNavBarItems(),
-        //currentIndex: _selectedIndex,
-        // selectedFontSize: 15,
-        backgroundColor: Colors.black87,
-      ),
-      // body: Body_Divide_Test(),
-    );
   }
 
   void bottomTapped(int index){
@@ -122,7 +54,137 @@ class _HomeState extends State<Home>{
       }
     });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: pageController,
+      physics: ClampingScrollPhysics(),
+      onPageChanged: (index) {
+        pageChanged(index);
+      },
+      children: [
+        Body(pageIndex: _selectedPage, bottomTapped: bottomTapped),
+        // Discover(pageIndex: _selectedIndex, bottomTapped: bottomTapped)
+      ]
+    );
+  }
 }
+// class Home extends StatefulWidget{
+//   final String title;
+
+//   Home({Key key, this.title}) : super(key: key);
+//   @override
+//   State<StatefulWidget> createState()
+//   => _HomeState();
+// }
+
+// class _HomeState extends State<Home>{
+//   String _title;
+//   bool _showStar = true;
+//   int _selectedPage = 0;
+//   List<Widget> pageList = List<Widget>();
+
+//   @override
+//   void initState() {
+//     pageList.add(Body());
+//     pageList.add(Discover());
+//     _title = "MY GAMES";
+//     super.initState();
+//   }
+
+//   List<BottomNavigationBarItem> buildBottomNavBarItems(){
+//     return [
+//       BottomNavigationBarItem(
+//         icon: Icon(IconMoon.igamepad),
+//         title: Text('My Games'),
+//       ),
+//       BottomNavigationBarItem(
+//         icon: Icon(IconMoon.isearch),
+//         title: Text('Discover'),
+//       ),
+//     ];
+//   }
+
+//   PageController pageController = PageController(
+//     initialPage: 0,
+//     keepPage: true,
+//   );
+
+//   Widget buildPageView(){
+//     return PageView(
+//         controller: pageController,
+//         onPageChanged: (index){
+//           pageChanged(index);
+//         },
+//         //scrollDirection: Axis.horizontal,
+//         children: <Widget>[
+//           Body(),
+//           Discover(),
+//         ]
+//     );
+//   }
+
+//   void pageChanged(int index){
+//     setState((){
+//       _selectedPage = index;
+//     });
+//   }
+
+//   void onAddButtonTapped(int index){
+//     pageController.animateToPage(index);
+//     pageController.jumpToPage(index);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       drawer: SideMenu(),
+//       appBar: HomeAppbar(_title, _showStar, context),
+//       body: buildPageView(),
+//       bottomNavigationBar: BottomNavigationBar(
+
+//         type: BottomNavigationBarType.fixed,
+//         selectedItemColor: Colors.white,
+//         unselectedItemColor: Colors.white38,
+//         // onTap: _onItemTapped,
+//         iconSize: 50,
+//         currentIndex: _selectedPage,
+//         onTap: (index){
+//           bottomTapped(index);
+//         },
+//         items: buildBottomNavBarItems(),
+//         //currentIndex: _selectedIndex,
+//         // selectedFontSize: 15,
+//         backgroundColor: Colors.black87,
+//       ),
+//       // body: Body_Divide_Test(),
+//     );
+//   }
+
+//   void bottomTapped(int index){
+//     setState((){
+//       _selectedPage = index;
+//       pageController.animateToPage(index, duration: Duration(milliseconds: 700), curve: Curves.ease);
+//       switch(index){
+//         case 0: {
+//           setState(() {
+//             _showStar = true;
+//             _title="MY GAMES";
+//           });
+//           break;
+//         }
+//         case 1:{
+//           setState(() {
+//             _showStar = false;
+//             _title="DISCOVER";
+//           });
+//           break;
+//         }
+//       }
+//     });
+//   }
+// }
 
 // class FavoriteItem {
 //   bool isFavor = false;
