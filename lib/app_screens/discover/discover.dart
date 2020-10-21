f58dbff0eb8b9b5faf969a8dc5ff362ff0c91d46
GeonwooVincentKim <kdsnop@gmail.com>
2020-10-21 13:57:40 +0900

    Testing...

--- lib/app_screens/discover/discover.dart
+++ lib/app_screens/discover/discover.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'discover/discover_attributes/DiscoverBody.dart';
+import 'package:flutter_app/app_screens/discover/discover_attributes/DiscoverBody.dart';
 
 // ignore: non_constant_identifier_names
 void discover_main(){
