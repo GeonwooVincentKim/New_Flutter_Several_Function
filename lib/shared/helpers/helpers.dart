a8047d61b3c3724be94eb91d2e366f77b8ec44d7
GeonwooVincentKim <kdsnop@gmail.com>
2020-11-12 14:59:48 +0900

    Some codes I didn't comment.

--- lib/shared/helpers/helpers.dart
+++ lib/shared/helpers/helpers.dart
@@ -11,11 +11,18 @@ String getRandomString(int length){
 }
 
 DateTime getDateTimeFormat(String currentDate){
+  // if(currentDate.contains('/')) {
+  //   final String stringDate = currentDate.replaceAll('/', '-');
+  //   return DateTime.parse(stringDate);
+  // }else{
+  //   final currentDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+  //   return DateTime.parse(currentDate);
+  // }
   if(currentDate.contains('/')) {
     final String stringDate = currentDate.replaceAll('/', '-');
     return DateTime.parse(stringDate);
-  }else{
-    final currentDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
-    return DateTime.parse(currentDate);
+  } else {
+    final DateTime today = DateTime.now();
+    return DateTime.parse('$currentDate-${today.month + 1}-01');
   }
 }
